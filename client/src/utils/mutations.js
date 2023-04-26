6e2c4c16963463f0cb38d60ceb7bcbd959eb9111
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-26 14:32:50 +1000

    A. clientside mutations.js; login/create user

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $name, email: $email, password: $password) {
+      token
+    }
+  }
+`;
