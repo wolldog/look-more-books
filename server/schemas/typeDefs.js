0852d7f0a79dd58c35dc4937c426cb07cff8d880
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-26 13:46:55 +1000

    M. typeDefs: + model and Auth types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    description: String
+    bookID: String
+    image: String
+    link: String
+    title: String
+    authors: [String]
+  }
+
+  # Auth type to handle returning data from a profile creating or user login
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    getMe: User
+  }
+
+  type Mutation {
+    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
+    createUser(username: String!, email: String!, password: String!): Auth
+    loginUser(email: String!, password: String!): Auth
+  }
+  `;
+
+  module.exports = typeDefs;
