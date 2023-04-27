1b7dac98e082e29bb8ab64b41ac82463e8ac9e5a
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-27 16:54:09 +1000

    M. App.js; amended SavedBooks path

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ function App() {
             element={ <SearchBooks /> }
             />
           <Route 
-            path="/" 
+            path="/saved" 
             element={ <SavedBooks /> }
             />
         </Routes>
