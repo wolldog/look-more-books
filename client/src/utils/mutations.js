97dc21933d7acf4d52bdcf984e96a88bf721353a
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-28 14:30:24 +1000

    M. utils/mutations.js; add SAVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,21 +1,39 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-mutation LoginUser($email: String!, $password: String!) {
-  loginUser(email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}
 `;
 
 export const CREATE_USER = gql`
   mutation createUser($username: String!, $email: String!, $password: String!) {
     createUser(username: $username, email: $email, password: $password) {
       token
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookDataInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookID
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
