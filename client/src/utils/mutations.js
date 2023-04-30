ae351f9be0c628eefdfcc9699dd569da3791be07
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-30 20:37:01 +1000

    M.utils/mutations; add REMOVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -37,3 +37,22 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
